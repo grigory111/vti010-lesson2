380bc5cc35b7640f88990192db80800b1d786908
SemenovGE <SemenovGE@ors-wkst0295.mai.ru>
2018-02-24 19:04:37 +0300

    hjhjh

--- univer/src/main/java/mai/lesson_3/univer/App.java
+++ univer/src/main/java/mai/lesson_3/univer/App.java
@@ -1,5 +1,7 @@
 package mai.lesson_3.univer;
 
+import mai.lesson_3.univer.storage.JdbcStorage;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+       JdbcStorage stor = new JdbcStorage();
     }
 }
